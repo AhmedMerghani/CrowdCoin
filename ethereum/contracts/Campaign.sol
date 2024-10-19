ad8bef3e5f9f3fd3049dc01cc931865ceced938a
AhmedMerghani <ahmed-merghani@outlook.com>
2024-10-19 23:15:43 +0400

    Initial Commit

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint256 minimum) public {
+        address newCampaign = address(new Campaign(minimum, msg.sender));
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[] memory) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint256 value;
+        address recipient;
+        bool complete;
+        uint256 approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    address public manager;
+    uint256 public minimumContribution;
+    Request[] public requests;
+    mapping(address => bool) public approvers;
+    uint256 public approversCount;
+
+    constructor(uint256 minimum, address creator) public {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address recipient
+    ) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+
+        requests.push(newRequest);
+    }
+
+    function approveRequest(uint256 index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint256 index) public restricted {
+        Request storage request = requests[index];
+
+        require(!request.complete, "Request already completed.");
+        require(
+            request.approvalCount > approversCount / 2,
+            "Not enough approvals."
+        );
+
+        address payable recipient = address(uint160(request.recipient));
+        require(recipient.send(request.value), "Transfer failed.");
+
+        request.complete = true;
+    }
+}
