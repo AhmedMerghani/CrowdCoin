cfca063e7567b9925850ec59151bb7be9692c9ab
AhmedMerghani <ahmed-merghani@outlook.com>
2024-10-26 14:09:18 +0400

    Showing the open campaigns in cards and a button to create a campaign

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory";
+import { Button, Card } from "semantic-ui-react";
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
+  static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log(campaigns);
+    return { campaigns };
+  }
+
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+
+    return <Card.Group items={items} />;
   }
 
   render() {
-    return <div>Campaigns Index!</div>;
+    return (
+      <div>
+        <link
+          async
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/semantic-ui@latest/dist/semantic.min.css"
+        />
+        <h3>Open Campaigns</h3>
+        {this.renderCampaigns()}
+        <Button content="Create Campaign" icon="add circle" primary />
+      </div>
+    );
   }
 }
 
